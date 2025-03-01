48528e1f81eb64c98bc57c62095b0749b84681d2
Perryman <perryman@users.noreply.github.com>
2025-03-01 01:55:40 -0500

    clean up unused files

--- utils/iterable_helpers.py
+++
@@ -1,6 +0,0 @@
-def find(iterable, pred):
-    """
-    Returns the first element in 'iterable' for which 'pred' returns True.
-    If no element is found, returns None.
-    """
-    return next((element for element in iterable if pred(element)), None)
